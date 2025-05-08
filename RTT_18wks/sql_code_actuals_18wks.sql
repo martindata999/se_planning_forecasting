7da9561ea0665ab5d3a99d3d96e14c13231b76a6
martindata999 <martin.bloyce2@nhs.net>
2025-05-08 16:49:04 +0100

    Narrowed date pull for post-Covid

--- RTT_18wks/sql_code_actuals_18wks.sql
+++ RTT_18wks/sql_code_actuals_18wks.sql
@@ -5,6 +5,7 @@ FROM (
     FROM [UDALLakeMart_PATPlanningReporting].[actual_data_2526_1]
     WHERE [RegionCode] = 'Y59'
     AND [PlanningRef] IN ('E.B.40', 'E.B.3a')
+    AND [DimDateStart] >= '2022-10-01'
 ) AS SourceTable
 PIVOT (
     SUM([Value]) FOR [PlanningRef] IN ([E.B.40], [E.B.3a])
